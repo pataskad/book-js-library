442785cf3407363d415f48571a6dca5ddc6a168f
Dallas Pataska <pataskad@gmail.com>
2021-08-27 11:55:45 -0500

    Book visual display formatting added, js loop added via template literals

--- index.js
+++ index.js
@@ -5,38 +5,37 @@ let myLibrary = [
         title: 'Harry Potter',
         author: 'J.K. Rowling',
         pages: '560',
-        hasRead: false,
+        hasRead: 'No',
     },
     {
         title: 'Game of Thrones',
         author: 'G.R.R Martin',
         pages: '1002',
-        hasRead: false,
+        hasRead: 'No',
     }
 ]
 
 const container = document.querySelector('.container')
-const card = document.querySelector('.card')
 
 function Book() { // constructor, need params?
     this.title = title
     this.author = author
     this.pages = pages
-    hasRead = false;
+    hasRead = 'No';
 }
 
 function addBookToLibrary() {
-    const book = Object.create(Book)
+    const book = new Book()
     myLibary.push(book)
     console.log(book.title, book.author, book.pages, hasRead)
 }
 
 for (let i = 0; i < myLibrary.length; i++) {
-    // made a card template within this loop?
-    // add 'card' class
-    // reference 'etch a sketch' project and others as needed to create card within JS
-/*     const card = document.createElement('div')
-    card.classList.add('card') */ 
-    card.textContent += myLibrary[i].title + '\n' + myLibrary[i].author + '\n'
-    console.log(myLibrary[i])
+    const card = document.createElement('div')
+    card.classList.add('card')
+    container.appendChild(card)
+    card.innerHTML += '<h4>Title:</h4>' + '<br>' + `<p class='card-values'>${myLibrary[i].title}</p>` 
+          + '<br>' + '<h4>Author:</h4>' + '<br>' + `<p class='card-values'>${myLibrary[i].author}</p>`
+          + '<br>' + '<h4>Page Count:</h4>' + '<br>' + `<p class='card-values'>${myLibrary[i].pages}</p>`
+          + '<br>' + '<h4>Read:</h4>' + '<br>' + `<p class='card-values'>${myLibrary[i].hasRead}</p>`
 }
